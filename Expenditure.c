7639d99f8ed936822d890b0f7dc387620f8d41dc
sowjanyavangala <147488384+sowjanyavangala@users.noreply.github.com>
2023-12-27 13:22:19 +0530

    #include<stdio.h> int main() {     int x,y,n;     scanf("%d%d",&x,&y);     n=y*30;     if(x>=n)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,n;
+    scanf("%d%d",&x,&y);
+    n=y*30;
+    if(x>=n)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
