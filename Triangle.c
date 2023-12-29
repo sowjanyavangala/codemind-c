eb705b7ae5d1c3267f22b78c3117800271c03336
sowjanyavangala <147488384+sowjanyavangala@users.noreply.github.com>
2023-12-29 15:05:39 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(x==y&&y==z&&z==x)     {         printf("Equilateral triangle");     }     else if(x==y||y==z||z==x)     {         printf("Isosceles triangle");     }     else     {         printf("Scalene triangle");     } }

---
+++ Triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(x==y&&y==z&&z==x)
+    {
+        printf("Equilateral triangle");
+    }
+    else if(x==y||y==z||z==x)
+    {
+        printf("Isosceles triangle");
+    }
+    else
+    {
+        printf("Scalene triangle");
+    }
+}
