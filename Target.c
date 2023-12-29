6a1fe25120f560ef510ad3d012700c38520da14b
sowjanyavangala <147488384+sowjanyavangala@users.noreply.github.com>
2023-12-29 15:00:08 +0530

    #include<stdio.h> int main() {     int a,b,x,y;     scanf("%d%d%d%d",&a,&b,&x,&y);     if(a>=10&&b>=10&&x>=10&&y>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    if(a>=10&&b>=10&&x>=10&&y>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
